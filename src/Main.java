66123f21c139e3e17edff927abb11865fc410870
alexander <kabanov@inbox.ru>
2022-09-04 01:14:35 +0400

    Выполнил ДЗ "Условный оператор 2"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,87 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" Задание 1");
+        task1();
+        System.out.println("\n" + " Задание 2");
+        task2();
+        System.out.println("\n" + " Задание 3");
+        task3();
+        System.out.println("\n" + " Задание 4");
+        task4();
+        System.out.println("\n" + " Задание 5");
+        task5();
+    }
+
+    public static void task1() {
+        int clientOS = 1;
+        if (clientOS == 0)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        else if (clientOS == 1) System.out.println("Установите версию приложения для Android по ссылке");
+    }
+
+    public static void task2() {
+        int clientOS = 0;
+        int clientDeviceYear = 2012;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            else System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            else System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3() {
+        int year = 2012;
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0 && year != 2100) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void task4() {
+//int deliveryWithin =0;
+        int deliveryPeriod = 1;
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryPeriod);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            deliveryPeriod += 1;
+            System.out.println("Потребуется дней: " + deliveryPeriod);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            deliveryPeriod += 2;
+            System.out.println("Потребуется дней: " + deliveryPeriod);
+        }
+    }
+
+    public static void task5() {
+        int monthNumber = 9;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("такого месяца не существует");
+        }
     }
 }
